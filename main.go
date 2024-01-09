c61c76fb8299ad7f2e32b52eb4fe0710f9965b30
Israel De La Cruz <israelz11@hotmail.com>
2024-01-09 11:43:59 -0600

    Siguiente commit

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/israelz11/godesde0/variables"
 )
 
 func main() {
 
-	variables.MostrarEnteros()
+	//variables.MostrarEnteros()
+	//variables.RestoVariables()
+	estado, texto := variables.ConvierteATexto(1599)
 
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
